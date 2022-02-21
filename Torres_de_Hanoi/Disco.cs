b9eade6bb1310d6fbbffaee3110c1a2ed1bfdade
alvaroescr0008 <72156197+alvaroescr0008@users.noreply.github.com>
2022-02-21 16:39:17 +0100

    Primer commit, con las dos clases creadas

---
+++ Torres_de_Hanoi/Disco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torres_de_Hanoi
+{
+    class Disco
+    {
+        //------------------------------------------------------------------------
+                //------------------------------------------------------------------------
+
+
+        private int numero;
+    }
+}
---
+++ Torres_de_Hanoi/Pila.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Torres_de_Hanoi
+{
+    class Pila
+    {
+        public int Size { get; set; }
+           //------------------------------------------------------------------------
+                //------------------------------------------------------------------------
+
+      
+
+        private int Top;
+                //------------------------------------------------------------------------
+        //------------------------------------------------------------------------
+
+                //------------------------------------------------------------------------
+        //------------------------------------------------------------------------
+
+        public Pila(int Top){
+
+            this.Top = Top;
+            
+
+        }
+
+
+                //------------------------------------------------------------------------
+        //------------------------------------------------------------------------
+
+
+        public int Top{
+            get
+            {
+                return this.Top;
+            }
+
+            set
+            {
+              
+                    this.Top = value;
+                    
+            }
+        }
+
+        public List<Disco> Elementos { get; set; }
+
+        /* TODO: Elegir tipo de Top
+        public int Top { get; set; }
+        public String Top { get; set; }        
+        */
+        /* TODO: Elegir tipo de Elementos
+        public Disco[] Elementos { get; set; }
+        public List<Disco> Elementos { get; set; }
+        */
+
+        /* TODO: Implementar métodos */
+
+
+        public void push(Disco d)
+        {
+
+        }
+
+        public Disco pop()
+        {
+            return null;
+        }                
+
+        public bool isEmpty()
+        {
+            return true;
+        }
+
+    }
+}
